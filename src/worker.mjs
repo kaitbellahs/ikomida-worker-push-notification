5fb814eec621ec73819c643e356d601a2e424078
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-02 00:59:03 -0300

    Update

--- src/worker.mjs
+++ src/worker.mjs
@@ -4,11 +4,13 @@ import {
     RabbitMQ,
     SqlDB,
     GoogleAdmin,
-    Logger
-} from 'ikomida-shared';
+    Logger,
+    Roles,
+    deepCopy
+} from 'ikomida-shared'
 import {
     createRequire
-} from "module";
+} from "module"
 const require = createRequire(
     import.meta.url)
 let {
@@ -21,8 +23,8 @@ name = name
 
 class PushNotificationWorker {
 
-    googleAdmin;
-    amqp;
+    googleAdmin
+    amqp
     logger
 
     constructor() {
@@ -31,7 +33,7 @@ class PushNotificationWorker {
 
     async run() {
         try {
-            this.googleAdmin = new GoogleAdmin()
+            this.googleAdmin = new GoogleAdmin(this.logger)
             this.amqp = new RabbitMQ(this.logger)
             await this.amqp.listenToMessages(RabbitMQ.PUSH_NOTIFICATION_SEVERITY, this.processMessages.bind(this))
         } catch (error) {
@@ -39,81 +41,126 @@ class PushNotificationWorker {
         }
     }
 
-    async processMessages(message, channel) {
+    async processMessages(payload, channel) {
         try {
-            this.logger.log(` [x] ${message.fields.routingKey}: message received: '${message.content.toString('utf8')}'`)
-            const messageObject = JSON.parse(message.content.toString('utf8'))
+            this.logger.log(` [x] ${payload.fields.routingKey}: payload received: '${payload.content.toString('utf8')}'`)
+            const messageObject = JSON.parse(payload.content.toString('utf8'))
             if (messageObject.method === 'send') {
                 const contractModel = await SqlDB.ContractModel.findOne({
                     where: {
-                        id: messageObject.object.contractId
-                    }
-                })
+                        id: messageObject.object.contractId,
+                        
+                    },
+                    include: [{
+                        model: SqlDB.PushNotificationModel,
+                        where: {
+                            role: Roles.VENDOR,
+                            
+                        },
+                        required: false,
+                    }]
+                }
+                )
 
                 if (!contractModel) {
-                    return false;
+                    this.logger.log(` [Erro]: Não foi localizado um estabelecimento!!`)
+                    return false
                 }
                 let pushNotificationModel
+                let userModel
                 if (messageObject.object.userId) {
                     const userModels = await contractModel.getUsers({
                         where: {
-                            id: messageObject.object.userId
-                        }
+                            id: messageObject.object.userId,
+                            
+                        },
+                        include: [{
+                            model: SqlDB.PushNotificationModel,
+                            where: {
+                                
+                            },
+                            required: false,
+                        }]
                     })
 
                     if (userModels.length !== 1) {
-                        return false;
+                        this.logger.log(` [Erro]: Não foi possível localizar o usuário para o envio de push notification!!`)
+                        return false
                     }
-                    const userModel = userModels[0];
-                    pushNotificationModel = await userModel.getPushNotificationModel()
+                    userModel = userModels?.[0]
+                    pushNotificationModel = userModel?.pushNotification
                 } else {
-                    const pushNotificationModels = await contractModel.getPushNotifications({
-                        where: {
-                            role: Roles.VENDOR
-                        }
+                    const pushNotificationModels = contractModel?.pushNotifications
+                    if ((pushNotificationModels?.length ?? 0) !== 1) {
+                        this.logger.log(` [Erro]: foi localizado mais de um push notification token no sistema!`)
+                        return false
+                    }
+                    pushNotificationModel = pushNotificationModels[0]
+                }
+                if (pushNotificationModel) {
+                    const message = deepCopy(messageObject?.object?.message)
+                    const pushNotificationMessageModel = await pushNotificationModel.createPushNotificationMessage({
+                        title: message?.notification?.title,
+                        body: message?.notification?.body,
+                        data: JSON.stringify(message?.data)
                     })
-                    if ((pushNotificationModels?.length ?? 0) === 1) {
-                        pushNotificationModel = pushNotificationModels[0];
+
+                    await pushNotificationMessageModel.setContract(contractModel)
+
+                    if (messageObject.object.userId) {
+                        await pushNotificationMessageModel.setUser(userModel)
                     }
+
+                    let i = 1
+                    let seconds = new Date().getTime()
+                    do {
+                        message.token = pushNotificationModel?.token
+                        i++
+                        const response = await this.sendPushNotificationByToken(pushNotificationMessageModel, message)
+                        if (response) {
+                            this.logger.log(` [x] Push notification enviado com sucesso`)
+                            channel.ack(payload)
+                            return true
+                        }
+                        if (response === false) {
+                            this.logger.warn(` [x] Push notification não foi enviado, token não foi localizado`)
+                            pushNotificationModel?.destroy()
+                            channel.ack(payload)
+                            return true
+                        }
+                        if (i < 3) {
+                            seconds += i
+                            await this.sleep(i * 1000)
+                        }
+                    } while (i < 4)
+                    this.logger.log(` [Erro]: O Push notification não foi enviado apos ${i} tentativas wm ${(new Date().getTime() - seconds) / 1000} segundos!`)
+                } else {
+                    this.logger.log(` [Erro]: Não foi possível localizar token do usuário ou dispositivo para o envio de push notification!!`)
                 }
-                const pushNotificationMessageModel = await pushNotificationModel.createPushNotificationMessage({
-                    title: message?.notification?.title,
-                    body: message?.notification?.body,
-                    data: JSON.stringify(message?.data)
-                })
-
-                await pushNotificationMessageModel.setContract(contractModel)
-
-                if (messageObject.object.userId) {
-                    await pushNotificationMessageModel.setUser(userModel)
-                }
-
-                for (let i = 1; i < 4; i++) {
-                    if (this.sendPushNotificationByToken(pushNotificationMessageModel, messageObject.object.message)) {
-                        break;
-                    }
-                    await this.sleep(i * 1000)
-                }
-
+            } else {
+                this.logger.log(` [Erro]: O metodo: ${messageObject?.method} não suportado!`)
             }
         } catch (error) {
             this.logger.error(error)
-        } finally {
-            channel.ack(message)
         }
+        return false
     }
 
-    async sendPushNotificationByToken(model, message) {
-
-        const response = await this.googleAdmin.sendPushNotification(message)
-        if (!response) {
-            return false;
+    async sendPushNotificationByToken(model, payload) {
+        try {
+            const response = await this.googleAdmin.sendPushNotification(payload)
+            if (!response) {
+                return false
+            }
+            model.remoteId = response
+            model.send = true
+            model.save()
+        } catch (error) {
+            this.logger.log("payload:", payload)
+            this.logger.error(error)
+            return false
         }
-        model.remoteId = response;
-        model.send = true;
-        model.save()
-
-        return true;
+        return true
     }
 
     async sleep(ms) {
