5c63ca837c486e810b8370d6ce9440834cda5fe3
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-13 11:56:31 -0300

    Suport send pn to multi vendors
    Update libs

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,7 @@
 import { Domain, DBModels, Types, BackendTypes, Utils } from '@ikomida/shared-backend'
 import { Message, Channel } from 'amqplib'
 import { createRequire } from 'module'
+import { Includeable } from 'sequelize'
 const require = createRequire(import.meta.url)
 let { name } = require('../package.json')
 name = name
@@ -33,23 +34,40 @@ class PushNotificationWorker {
       this.logger.log(` [x] ${message.fields.routingKey}: message received: '${message.content.toString('utf8')}'`)
       const payload: Types.Classes.CAMQPPayload<Types.Classes.CAMQPPayloadObject> =
         Types.Classes.CAMQPPayload.fromObject(JSON.parse(message.content.toString('utf8')))
+      console.log('payload:', payload)
       const payloadObject: Types.Classes.CAMQPPayloadObject = Types.Classes.CAMQPPayloadObject.fromObject(
         payload.object
       )
+      console.log('payloadObject:', payloadObject)
       if (payload.method === 'send') {
+        const include: Includeable = payloadObject.userId
+          ? {
+            model: DBModels.UserModel,
+            where: {
+              id: payloadObject.userId
+            },
+            include: [
+              {
+                model: DBModels.PNModel,
+                required: false
+              }
+            ],
+            required: false
+          }
+          : {
+            model: DBModels.PNModel,
+            where: {
+              role: {
+                [Domain.SqlDB.Op.in]: [BackendTypes.Roles.VENDOR, BackendTypes.Roles.STAFF]
+              }
+            },
+            required: false
+          }
         const contractModel = await DBModels.ContractModel.findOne({
           where: {
             id: payloadObject.contractId
           },
-          include: [
-            {
-              model: DBModels.PNModel,
-              where: {
-                role: BackendTypes.Roles.VENDOR
-              },
-              required: false
-            }
-          ]
+          include
         })
 
         if (!contractModel) {
@@ -57,41 +75,38 @@ class PushNotificationWorker {
           channel.ack(message)
           return false
         }
-        let pNModel
+        let pNModels
         let userModel
         if (payloadObject.userId) {
-          const userModels = await contractModel.$get('users', {
-            where: {
-              id: payloadObject.userId
-            },
-            include: [
-              {
-                model: DBModels.PNModel,
-                where: {},
-                required: false
-              }
-            ]
-          })
-
-          if (userModels.length !== 1) {
+          const userModels = contractModel.users
+          if (userModels?.length !== 1) {
             this.logger.log(` [Erro]: Não foi possível localizar o usuário para o envio de push notification!!`)
             channel.ack(message)
             return false
           }
           userModel = userModels?.[0]
-          pNModel = userModel?.pN
+          pNModels = [userModel?.pN]
         } else {
-          const pNModels = contractModel?.pNs
-          if ((pNModels?.length ?? 0) !== 1) {
-            this.logger.log(` [Erro]: foi localizado mais de um push notification token no sistema!`)
-            return false
-          }
-          pNModel = pNModels?.[0]
+          pNModels = contractModel.pNs
         }
-        if (pNModel) {
-          const pNmessage: Types.Classes.CNotificationPayload = Types.Classes.CNotificationPayload.fromObject(
-            payloadObject.message
+        if (!pNModels) {
+          this.logger.log(
+            ` [Erro]: Não foi possível localizar token do usuário ou dispositivo para o envio de push notification!!`
           )
+          channel.ack(message)
+          return false
+        }
+        const pNmessage: Types.Classes.CNotificationPayload = Types.Classes.CNotificationPayload.fromObject(
+          payloadObject.message
+        )
+        console.log('pNmessage:', pNmessage)
+        for (const pNModel of pNModels) {
+          if (!pNModel) {
+            this.logger.log(
+              ` [Erro]: Não foi possível localizar token do usuário ou dispositivo para o envio de push notification!!`
+            )
+            continue
+          }
           const pNMessageModel = await pNModel.$create('pNMessage', {
             title: pNmessage?.notification?.title,
             body: pNmessage.notification?.body,
@@ -106,9 +121,9 @@ class PushNotificationWorker {
             pNmessage.token = pNModel?.token
             pNmessage.id = pNMessageModel?.id
             pNmessage.priority = 10
-            pNmessage.ikomidaId = contractModel?.ikomidaID
+            pNmessage.ikomidaId = contractModel.ikomidaID
             i++
-            const response = await this.sendPushNotificationByToken(pNMessageModel, pNmessage, pNModel?.platform)
+            const response = await this.sendPushNotificationByToken(pNMessageModel, pNmessage, pNModel.platform)
             switch (response?.code) {
               case 0:
                 this.logger.log(` [x] Push notification enviado com sucesso`)
@@ -131,16 +146,9 @@ class PushNotificationWorker {
             }
           } while (i < 4)
           this.logger.log(
-            ` [Erro]: O Push notification não foi enviado após ${i} tentativas wm ${
-              (new Date().getTime() - seconds) / 1000
+            ` [Erro]: O Push notification não foi enviado após ${i} tentativas wm ${(new Date().getTime() - seconds) / 1000
             } segundos!`
           )
-        } else {
-          this.logger.log(
-            ` [Erro]: Não foi possível localizar token do usuário ou dispositivo para o envio de push notification!!`
-          )
-          channel.ack(message)
-          return false
         }
       } else {
         this.logger.log(` [Erro]: O metodo: ${payload?.method} não suportado!`)
